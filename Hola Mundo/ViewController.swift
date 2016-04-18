d3db05e12ef572b41716d3a82d0e91057e9b324e
Hkmoca <hmoreno@nearsoft.com>
2016-04-18 15:14:38 -0700

    Initial Commit

---
+++ Hola Mundo/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Hola Mundo
+//
+//  Created by Héctor Moreno on 16/04/16.
+//  Copyright © 2016 Hkapp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var nameExitField: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func textView(sender: AnyObject) {
+        nameExitField.text = "Hola \(nameTextField.text!)"
+        view.endEditing(true)
+    }
+
+}
+
